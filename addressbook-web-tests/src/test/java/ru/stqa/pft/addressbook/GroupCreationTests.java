d776d17eced2247dc11bc1a1e3bb160b3ea8e40a
haim <nikiforov.denis@gmail.com>
2018-03-11 21:58:18 +0200

    add TestBase class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,88 +1,18 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
 
 import org.testng.annotations.*;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupCreationTests {
-	FirefoxDriver wd;
-
-//  private WebDriver wd;
-//  private String baseUrl;
-//  private boolean acceptNextAlert = true;
-//  private StringBuffer verificationErrors = new StringBuffer();
-
-	@BeforeMethod(alwaysRun = true)
-	public void setUp() throws Exception {
-		System.setProperty("webdriver.gecko.driver", "/Users/denisnikiforov/code/java_pft/addressbook-web-tests/src/test/resources/webdrivers/geckodriver/macos/geckodriver");
-		wd = new FirefoxDriver();
-		wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-		wd.get("http://addressbook.local:8888/index.php");
-		login("admin", "secret");
-
-	}
-
-	private void login(String username, String password) {
-		wd.findElement(By.name("user")).click();
-		wd.findElement(By.name("user")).clear();
-		wd.findElement(By.name("user")).sendKeys(username);
-		wd.findElement(By.name("pass")).click();
-		wd.findElement(By.name("pass")).clear();
-		wd.findElement(By.name("pass")).sendKeys(password);
-		wd.findElement(By.xpath("//input[@value='Login']")).click();
-	}
+public class GroupCreationTests extends TestBase {
 
 	@Test
 	public void test_group_creation() throws Exception {
-		GoToGroupPage();
+		goToGroupPage();
 		initGroupCreation();
 		fillGroupForm(new GroupData("test1", "test2", "footer"));
 		submitGroupCreation();
-		returnToGroupPage();
-	}
-
-	private void returnToGroupPage() {
-		wd.findElement(By.linkText("group page")).click();
-	}
-
-	private void submitGroupCreation() {
-		wd.findElement(By.name("submit")).click();
-	}
-
-	private void fillGroupForm(GroupData groupData) {
-		wd.findElement(By.name("group_name")).click();
-		wd.findElement(By.name("group_name")).clear();
-		wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-		wd.findElement(By.name("group_header")).clear();
-		wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-		wd.findElement(By.name("group_footer")).clear();
-		wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-	}
-
-	private void initGroupCreation() {
-		wd.findElement(By.name("new")).click();
-	}
-
-	private void GoToGroupPage() {
-		wd.findElement(By.linkText("groups")).click();
-	}
-
-	@AfterClass(alwaysRun = true)
-	public void tearDown() throws Exception {
-		wd.quit();
-	}
-
-	public static boolean isAlertPresent(FirefoxDriver wd) {
-		try {
-			wd.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
+		goToGroupPage();
 	}
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
@@ -15,42 +15,15 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 
 
-public class UserCreationTests {
-	FirefoxDriver wd;
+public class UserCreationTests extends TestBase {
 
-
-//		private WebDriver driver;
-//		private String baseUrl;
-		private boolean acceptNextAlert = true;
-		private StringBuffer verificationErrors = new StringBuffer();
-
-		@BeforeClass(alwaysRun = true)
-		public void setUp() throws Exception {
-			System.setProperty("webdriver.gecko.driver", "/Users/denisnikiforov/code/java_pft/addressbook-web-tests/src/test/resources/webdrivers/geckodriver/macos/geckodriver");
-			wd = new FirefoxDriver();
-			wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			wd.get("http://addressbook.local:8888/index.php");
-			login("admin", "secret");
-
-		}
-
-		private void login(String username, String password) {
-		wd.findElement(By.name("user")).click();
-		wd.findElement(By.name("user")).clear();
-		wd.findElement(By.name("user")).sendKeys(username);
-		wd.findElement(By.name("pass")).click();
-		wd.findElement(By.name("pass")).clear();
-		wd.findElement(By.name("pass")).sendKeys(password);
-		wd.findElement(By.xpath("//input[@value='Login']")).click();
+	@Test
+	public void testUntitledTestCase() throws Exception {
+		initNewUser();
+		fillUserForm(new UserData("test1", "test2", "test3", "test4", "test5", "test7", "912987346", "9187364", "2345235", "2345325", "test@dsfs.com", "asdad@asd.com", "asldjaos@asodk.com", "https://google.com", "25", "July", "1984", "11", "October", "2018", "Norilsk", "home Norilsk", "notes test"));
+		submitUserCreation();
+		returnToHomePage();
 	}
-
-		@Test
-		public void testUntitledTestCase() throws Exception {
-			initNewUser();
-			fillUserForm(new UserData("test1", "test2", "test3", "test4", "test5", "test7", "912987346", "9187364", "2345235", "2345325", "test@dsfs.com", "asdad@asd.com", "asldjaos@asodk.com", "https://google.com", "25", "July", "1984", "11", "October", "2018", "Norilsk", "home Norilsk", "notes test"));
-			submitUserCreation();
-			returnToHomePage();
-		}
 
 	private void returnToHomePage() {
 		wd.get("http://addressbook.local:8888/index.php");
@@ -136,49 +109,5 @@ public class UserCreationTests {
 		wd.findElement(By.linkText("add new")).click();
 		wd.findElement(By.xpath("(//input[@name='quickadd'])[2]")).click();
 	}
-
-	@AfterClass(alwaysRun = true)
-		public void tearDown() throws Exception {
-			wd.quit();
-			String verificationErrorString = verificationErrors.toString();
-			if (!"".equals(verificationErrorString)) {
-				fail(verificationErrorString);
-			}
-		}
-
-		private boolean isElementPresent(By by) {
-			try {
-				wd.findElement(by);
-				return true;
-			} catch (NoSuchElementException e) {
-				return false;
-			}
-		}
-
-		private boolean isAlertPresent() {
-			try {
-				wd.switchTo().alert();
-				return true;
-			} catch (NoAlertPresentException e) {
-				return false;
-			}
-		}
-
-		private String closeAlertAndGetItsText() {
-			try {
-				Alert alert = wd.switchTo().alert();
-				String alertText = alert.getText();
-				if (acceptNextAlert) {
-					alert.accept();
-				} else {
-					alert.dismiss();
-				}
-				return alertText;
-			} finally {
-				acceptNextAlert = true;
-			}
-		}
-
-
 
 }
